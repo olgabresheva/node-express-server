e9c9ab864a33d73f7f121c410a36c959fc310abc
olgab <olgabresheva7@gmail.com>
2020-09-24 21:56:24 -0700

    uncommented limit and skip for userGetAll

--- src/modules/user/userGetAll.js
+++ src/modules/user/userGetAll.js
@@ -2,8 +2,8 @@ import User from '../models/user';
 
 export default function userGetAll(req, res) {
   User.find()
-    // .limit(2)
-    // .skip(2)
+    .limit(2)
+    .skip(2)
     .exec()
     .then((result) => {
       res.status(200).json(result);
